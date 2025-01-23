d95074abbed3a5074c47bade8a1c8ce9e7100822
Yannick-Kees <yannick@kees-daun.de>
2025-01-23 22:34:07 +0100

    20250123_2234

--- src/app.py
+++ src/app.py
@@ -2,108 +2,141 @@
 # Licensed under the MIT License. See LICENSE for details.
 
 """
-Description
+    Start application script
 """
 
-from flask import Flask, render_template, request, jsonify
 from datetime import datetime, timedelta
 from collections import defaultdict
 
-from lit import lit_list
+from flask import Flask, render_template, request, jsonify
 
+from lit import lit_list
 
 app = Flask(__name__)
 
-
 articles_db = {}  # This will hold article data by ID
-votes_db = defaultdict(lambda: {'upvotes': 0, 'downvotes': 0})  # Tracks votes for each article
+votes_db = defaultdict(
+    lambda: {"upvotes": 0, "downvotes": 0}
+)  # Tracks votes for each article
+
 
 def research_papers():
-    query = "(ISO AND 8800) OR (AI AND DevOps) OR (AI AND Safety) OR (Deep AND LEARNING AND Safety) OR (AI AND (Standart OR Norm)) "
-    
+    """
+    Define query to research papers
+    """
+    query = "(ISO AND 8800) OR (AI AND DevOps) OR (AI AND Safety) OR \
+            (Deep AND LEARNING AND Safety) OR (AI AND (Standart OR Norm)) "
+
     print(f" Searching for {query}")
     return lit_list(query)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    # By default, show all papers
+    """
+    Main page; by default, show all papers
+    """
     papers = research_papers()
+
     for article in papers:
+
         if article.id not in articles_db:
             articles_db[article.id] = {
-                'title': article.title,
-                'date': article.date,
-                'summary': article.summary,
-                'url': article.url,
-                'id': article.id,
-                'authors': article.authors
+                "title": article.title,
+                "date": article.date,
+                "summary": article.summary,
+                "url": article.url,
+                "id": article.id,
+                "authors": article.authors,
             }
-            
+
     articles_with_votes = [
         {
             **articles_db[article.id],
-            'upvotes': votes_db[article.id]['upvotes'],
-            'downvotes': votes_db[article.id]['downvotes'],
-            'score': votes_db[article.id]['upvotes'] - votes_db[article.id]['downvotes']
+            "upvotes": votes_db[article.id]["upvotes"],
+            "downvotes": votes_db[article.id]["downvotes"],
+            "score": votes_db[article.id]["upvotes"]
+            - votes_db[article.id]["downvotes"],
         }
         for article in papers
     ]
- 
-    return render_template('index.html', papers=articles_with_votes, filter_days=None)
 
-@app.route('/vote', methods=['POST'])
+    return render_template("index.html", papers=articles_with_votes, filter_days=None)
+
+
+@app.route("/vote", methods=["POST"])
 def vote():
+    """
+    Change Score of a paper
+    data: {
+        "article_title" : str
+        "vote_type": str
+    }
+    """
     data = request.json
-    article_title = data.get('article_title')
+    article_title = data.get("article_title")
     print(article_title)
-    vote_type = data.get('vote_type')
+    vote_type = data.get("vote_type")
 
     if article_title not in articles_db:
-        return jsonify({'error': 'Article not found'}), 404
+        return jsonify({"error": "Article not found"}), 404
 
-    if vote_type == 'upvote':
-        votes_db[article_title]['upvotes'] += 1
-    elif vote_type == 'downvote':
-        votes_db[article_title]['downvotes'] += 1
+    if vote_type == "upvote":
+        votes_db[article_title]["upvotes"] += 1
+    elif vote_type == "downvote":
+        votes_db[article_title]["downvotes"] += 1
     else:
-        return jsonify({'error': 'Invalid vote type'}), 400
+        return jsonify({"error": "Invalid vote type"}), 400
 
     # Return the updated score
-    score = votes_db[article_title]['upvotes'] - votes_db[article_title]['downvotes']
-    return jsonify({'score': score})
+    score = votes_db[article_title]["upvotes"] - votes_db[article_title]["downvotes"]
+    return jsonify({"score": score})
 
 
-@app.route('/filter', methods=['POST'])
+@app.route("/filter", methods=["POST"])
 def filter_papers():
-    # Get the filter value from the form
-    filter_days = int(request.form['filter_days'])
+    """
+    Get the filter value from the form
+    data = {
+        "filter_days": int
+    }
+    """
+
+    filter_days = int(request.form["filter_days"])
     papers = research_papers()
     cutoff_date = datetime.now() - timedelta(days=filter_days)
-    filtered_papers = [paper for paper in papers if paper.date.astimezone(None) >= cutoff_date.astimezone(None)]
-    
+    filtered_papers = [
+        paper
+        for paper in papers
+        if paper.date.astimezone(None) >= cutoff_date.astimezone(None)
+    ]
+
     for article in filtered_papers:
+
         if article.id not in articles_db:
             articles_db[article.id] = {
-                'title': article.title,
-                'date': article.date,
-                'summary': article.summary,
-                'url': article.url,
-                'id': article.id
+                "title": article.title,
+                "date": article.date,
+                "summary": article.summary,
+                "url": article.url,
+                "id": article.id,
             }
-            
+
     articles_with_votes = [
         {
             **articles_db[article.id],
-            'upvotes': votes_db[article.id]['upvotes'],
-            'downvotes': votes_db[article.id]['downvotes'],
-            'score': votes_db[article.id]['upvotes'] - votes_db[article.id]['downvotes']
+            "upvotes": votes_db[article.id]["upvotes"],
+            "downvotes": votes_db[article.id]["downvotes"],
+            "score": votes_db[article.id]["upvotes"]
+            - votes_db[article.id]["downvotes"],
         }
         for article in filtered_papers
     ]
 
-    return render_template('index.html', papers=articles_with_votes, filter_days=filter_days)
+    return render_template(
+        "index.html", papers=articles_with_votes, filter_days=filter_days
+    )
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=1405)
--- src/lit.py
+++ src/lit.py
@@ -2,35 +2,42 @@
 # Licensed under the MIT License. See LICENSE for details.
 
 """
-Description
+    Handle literature through arxive API
 """
 
-import arxiv 
 from datetime import datetime, timedelta
 
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+import arxiv
+
 
 class Paper:
+    """
+    Class to handle a single publication
+    """
+
     def __init__(self, title, date, summary, url, authors):
         self.title = title
         self.date = date
         self.summary = summary
         self.url = url
-        self.id = url.split('/')[-1].replace(".","-")
+        self.id = url.split("/")[-1].replace(".", "-")
         self.authors = authors
+
 
 def is_within_last_week(given_date):
     # Get the current date
     current_date = datetime.now()
-    
+
     # Calculate the date 7 days ago
     last_week = current_date - timedelta(days=7)
-    
+
     # Check if the given date is between last week and today
 
-    return last_week.astimezone(None) <= given_date.astimezone(None) <= current_date.astimezone(None)
-
+    return (
+        last_week.astimezone(None)
+        <= given_date.astimezone(None)
+        <= current_date.astimezone(None)
+    )
 
 
 def lit_list(query):
@@ -39,16 +46,22 @@ def lit_list(query):
 
     # Search for the 10 most recent articles matching the keyword "quantum."
     search = arxiv.Search(
-    query = query,
-    max_results = 20,
-    sort_by = arxiv.SortCriterion.SubmittedDate
+        query=query, max_results=20, sort_by=arxiv.SortCriterion.SubmittedDate
     )
 
     papers = []
 
     # `results` is a generator; you can iterate over its elements one by one...
     for r in client.results(search):
-        author_string=  "; ".join(author.name for author in r.authors)
-        papers.append(Paper(title=r.title, authors=author_string, date=r.published, summary=r.summary, url=str(r)))
-        
+        author_string = "; ".join(author.name for author in r.authors)
+        papers.append(
+            Paper(
+                title=r.title,
+                authors=author_string,
+                date=r.published,
+                summary=r.summary,
+                url=str(r),
+            )
+        )
+
     return papers
